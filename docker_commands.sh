3ad4fe815e66cc66b471baed4f761630c1abb67a
Marek <korzeniowskimarek.mk@gmail.com>
2025-01-10 08:08:35 +0100

    User route added.

--- docker_commands.sh
+++ docker_commands.sh
@@ -6,3 +6,14 @@ docker-compose exec api python -m pytest "src/tests"
 
 # Go to db
 docker-compose exec api-db psql -U postgres
+
+# Postgres cmd
+# \c api_dev
+# \dt
+#\q
+
+# Recreate db
+docker-compose exec api python manage.py recreate_db
+
+# seed db
+docker-compose exec api python manage.py seed_db
