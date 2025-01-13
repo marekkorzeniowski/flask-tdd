255da946cbd53117ca672962274986bdb629c41b
Marek <korzeniowskimarek.mk@gmail.com>
2025-01-13 08:09:42 +0100

    Update.

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,12 @@
 import os
 
 from flask import Flask  # new
+from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the db
 db = SQLAlchemy()
-
+admin = Admin(template_mode="bootstrap3")
 
 # new
 def create_app(script_info=None):
@@ -19,13 +20,15 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    if os.getenv("FLASK_ENV") == "development":
+        admin.init_app(app)
 
     # register blueprints
     from src.api.ping import ping_blueprint
 
     app.register_blueprint(ping_blueprint)
 
-    from src.api.users import users_blueprint
+    from src.api.users.views import users_blueprint
 
     app.register_blueprint(users_blueprint)
 
