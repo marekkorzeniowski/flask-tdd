fb4efc730745b6a2fb6b5ac5d654fb6fdb8dd62d
Marek <korzeniowskimarek.mk@gmail.com>
2025-01-27 08:54:24 +0100

    client service added to docker compose.

--- services/client/.eslintrc.cjs
+++ services/client/.eslintrc.cjs
@@ -5,14 +5,25 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended' // Add React rules
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react'], // Add React plugin
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/jsx-no-bind': ['error', {
+      'allowArrowFunctions': true,
+      'allowBind': false,
+      'ignoreRefs': true
+    }],
   },
-}
+  settings: {
+    react: {
+      version: '18.0',
+    },
+  },
+};
