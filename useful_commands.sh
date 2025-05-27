dd3327d266d619a4e458719380a20b1a22b797e1
Marek <korzeniowskimarek.mk@gmail.com>
2025-05-27 09:18:45 +0200

    buildspec added.

--- useful_commands.sh
+++ useful_commands.sh
@@ -29,3 +29,31 @@ aws ecr get-login-password --region eu-north-1 \
 docker push $FLASK_REACT_AWS_ACCOUNT_ID.dkr.ecr.eu-north-1.amazonaws.com/test-driven-users:dev
 
 docker push $FLASK_REACT_AWS_ACCOUNT_ID.dkr.ecr.eu-north-1.amazonaws.com/test-driven-client:dev
+
+
+# build images with prod tag and then push to ECR
+docker build \
+  -f services/users/Dockerfile.prod \
+  -t $FLASK_REACT_AWS_ACCOUNT_ID.dkr.ecr.eu-north-1.amazonaws.com/test-driven-users:prod \
+  ./services/users
+
+docker build \
+  -f services/client/Dockerfile.prod \
+  -t $FLASK_REACT_AWS_ACCOUNT_ID.dkr.ecr.eu-north-1.amazonaws.com/test-driven-client:prod \
+  --build-arg NODE_ENV=production \
+  --build-arg VITE_API_SERVICE_URL=${VITE_API_SERVICE_URL} \
+  ./services/client
+
+docker push $FLASK_REACT_AWS_ACCOUNT_ID.dkr.ecr.eu-north-1.amazonaws.com/test-driven-users:prod
+
+docker push $FLASK_REACT_AWS_ACCOUNT_ID.dkr.ecr.eu-north-1.amazonaws.com/test-driven-client:prod
+
+
+
+docker-compose exec -T api python -m pytest "src/tests" -p no:warnings --cov="src"
+docker-compose exec -T api flake8 src
+docker-compose exec -T api black src --check
+docker-compose exec -T api isort src --check-only
+docker-compose exec -T client npm run lint
+docker-compose exec -T client npm run prettier:check
+docker-compose exec -T client npm run prettier:write
