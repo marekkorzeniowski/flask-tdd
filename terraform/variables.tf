d47205d00a1f0d0c4268b11b42bed931ff765cfe
Marek <korzeniowskimarek.mk@gmail.com>
2025-06-04 15:18:11 +0200

    policies added

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,46 @@ variable "region" {
   description = "The AWS region to create resources in."
   default     = "eu-north-1"
 }
+
+# networking
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR Block for Public Subnet 1"
+  default     = "10.0.1.0/24"
+}
+variable "public_subnet_2_cidr" {
+  description = "CIDR Block for Public Subnet 2"
+  default     = "10.0.2.0/24"
+}
+variable "private_subnet_1_cidr" {
+  description = "CIDR Block for Private Subnet 1"
+  default     = "10.0.3.0/24"
+}
+variable "private_subnet_2_cidr" {
+  description = "CIDR Block for Private Subnet 2"
+  default     = "10.0.4.0/24"
+}
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["eu-north-1a", "eu-north-1c"]
+}
+
+# load balancer
+
+variable "health_check_path_client" {
+  description = "Health check path for the default target group"
+  default     = "/"
+}
+variable "health_check_path_users" {
+  description = "Health check path for the default target group"
+  default     = "/ping"
+}
+
+
+# ecs
+
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  default     = "flask-react-fargate"
+}
